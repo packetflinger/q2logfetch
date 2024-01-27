2ea18b06f32fca20eee0bd3bdc55fae929e28a6c
Joe Reid <joe@joereid.com>
2024-01-26 21:53:31 -0500

    oops, wrong command

--- main.go
+++ main.go
@@ -117,7 +117,7 @@ func runCommand(cmd string, host SSHConnection) ([]byte, error) {
 //
 // Local files are overwritten each time (not appending)
 func fetchLog(remoteFile string, localFile string, host SSHConnection) error {
-	output, err := runCommand("sudo truncate -s 0 "+remoteFile, host)
+	output, err := runCommand("cat "+remoteFile, host)
 	if err != nil {
 		return err
 	}
